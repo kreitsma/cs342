3a251186e3192779dab995a4cd9e36dce3b1bb89
kreitsma <aelic417@comcast.net>
2017-03-14 19:08:02 -0400

    Added guide

--- 07sql/guide7.sql
+++ 07sql/guide7.sql
@@ -16,3 +16,23 @@ WITH CHECK OPTION CONSTRAINT MoviesThisYearCnst;
 --v. Query modification is modifying or transforming the view query into a query on the underlying base tables.
 --    View materialization is physically creating a temporary or permanent view table when the first view is queried 
 --    and keeping that table on the assumption that other queries on the view will follow.
+
+--a.
+bladerunner <- SELECT_title='Blade Runner'(Movie)
+moviecast <- (bladerunner JOIN_id=movieId Casting)
+performers <- (moviecast JOIN_id=performerId Performer)
+RESULT <- PROJECT_title, firstName, lastName, status(Performer)
+RENAME_RESULT(BladeRunner)
+
+--b.
+{p.firstName, p.lastName | Performer(p) AND (Ec)(Casting(c) AND c.performerId = p.id AND status='star')}
+--E is supposed to be backwards E - gets all performers who have been stars
+
+--c.
+--An existential quantifier means the same thing as 'there exists/there is at least one'. So a formula F is true
+--if there exists a tuple that proves it true.
+
+--A universal quantifier means 'for all'. So a formula F is true if with tuple t, every tuple assigned to free occurences
+--of t in F is true, otherwise it is false.
+
+--safe expressions are guaranteed to yield a finite number of tuples.
