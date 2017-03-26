100621ad5c2ec0acf5b45725350cfc87d7625f54
kreitsma <aelic417@comcast.net>
2017-03-25 23:20:46 -0400

    Added homework

--- 06sql/homework06_1.sql
+++ 06sql/homework06_1.sql
@@ -8,5 +8,23 @@ SELECT TOP 10 m.employee_id, m.first_name, m.last_name, COUNT(e.employee_id) AS 
 --b.
 SELECT d.department_name, COUNT(e.employee_id) AS totalEmployees, SUM(e.salary) AS totalSalary
 	FROM Department d, Employees e, Locations l, Countries c
-	WHERE e.department_id = d.department_id AND 
+	WHERE l.department_id = d.department_id AND l.country_id = c.country_id 
+	AND c.country_name <> 'United States of America'
+	AND e.department_id = d.department_id
+	GROUP BY d.department_name
+	HAVING COUNT(*) < 100;
+	
+--c.
+SELECT d.department_name, m.first_name, m.last_name
+	FROM Departments d LEFT OUTER JOIN Employees m
+	ON d.manager_id = m.employee_id
+	JOIN Jobs j
+	ON m.job_id = j.job_id;
+	
+--d.
+SELECT d.department_name, AVG(e.salary)
+	FROM Departments d LEFT OUTER JOIN Employees e
+	ON d.department_id = e.department_id
+	GROUP BY d.department_name
+	ORDER BY AVG(e.salary) DESC;
  
