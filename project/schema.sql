92848f10a01f4f7a9e376fefa6631d842243c642
Kyle Reitsma <kr29@calvin.edu>
2017-03-31 12:50:00 -0400

    Added some project stuff

--- project/schema.sql
+++ project/schema.sql
@@ -1,28 +1,13 @@
 --Entity tables
---FOR EACH CATEGORY ATTRIBUTE:
---A 'w' means winner, 'n' means nominated, and NULL means not nominated for that category.
 
-create table Album (
+create table Artist (
 	ID integer PRIMARY KEY,
-	ArtistID integer,
-	name varchar(100),
-	yearNominated integer,
-	FOREIGN KEY ArtistID REFERENCES Artist(ID) ON DELETE CASCADE,
-	CHECK (yearNominated > 1900 AND yearNominated < 2018)
+	name varchar(100)
 	);
 	
 create table Label (
 	ID integer PRIMARY KEY,
 	name varchar(80)
-	);
-	
-create table Song (
-	ID integer PRIMARY KEY,
-	AlbumID integer,
-	title varchar(50),
-	yearNominated integer,
-	FOREIGN KEY AlbumID REFERENCES Album(ID) ON DELETE CASCADE,
-	CHECK (yearNominated > 1900 AND yearNominated < 2018)
 	);
 	
 create table Publisher (
@@ -31,16 +16,30 @@ create table Publisher (
 	adminBy varchar(80)
 	);
 	
-create table Artist (
-	ID integer PRIMARY KEY,
-	name varchar(100),
-	);
-	
 create table Person (
 	ID integer PRIMARY KEY,
 	fName varchar(30),
 	lName varchar(50),
 	nickName varchar(50)
+	);
+	
+create table Album (
+	ID integer PRIMARY KEY,
+	ArtistID integer,
+	name varchar(100),
+	yearNominated integer,
+	FOREIGN KEY (ArtistID) REFERENCES Artist(ID) ON DELETE CASCADE,
+	CHECK (yearNominated > 1900 AND yearNominated < 2018)
+	);
+	
+create table Song (
+	ID integer PRIMARY KEY,
+	AlbumID integer,
+	title varchar(50),
+	yearNominated integer,
+	FOREIGN KEY (AlbumID) REFERENCES Album(ID) ON DELETE CASCADE,
+	CHECK (yearNominated > 1900 AND yearNominated < 2018)
+	);
 	
 	
 create table Award ( --Get rid of ID and make year and category a super primary key?
@@ -51,9 +50,9 @@ create table Award ( --Get rid of ID and make year and category a super primary 
 	AlbumID integer,
 	SongID integer,
 	ArtistID integer,
-	FOREIGN KEY AlbumID REFERENCES Album(ID) ON DELETE CASCADE,
-	FOREIGN KEY SongID REFERENCES Song(ID) ON DELETE CASCADE,
-	FOREIGN KEY ArtistID REFERENCES Artist(ID) ON DELETE CASCADE
+	FOREIGN KEY (AlbumID) REFERENCES Album(ID) ON DELETE CASCADE,
+	FOREIGN KEY (SongID) REFERENCES Song(ID) ON DELETE CASCADE,
+	FOREIGN KEY (ArtistID) REFERENCES Artist(ID) ON DELETE CASCADE
 	);
 	
 create table AwardCategories (
@@ -65,46 +64,46 @@ create table AwardCategories (
 create table AlbumLabel (
 	AlbumID integer,
 	LabelID integer NOT NULL,
-	FOREIGN KEY AlbumID REFERENCES Album(ID) ON DELETE CASCADE,
-	FOREIGN KEY LabelID REFERENCES Label(ID) ON DELETE CASCADE
+	FOREIGN KEY (AlbumID) REFERENCES Album(ID) ON DELETE CASCADE,
+	FOREIGN KEY (LabelID) REFERENCES Label(ID) ON DELETE CASCADE
 	);
 	
 create table SongLabel (
 	SongID integer,
 	LabelID integer NOT NULL,
-	FOREIGN KEY SongID REFERENCES Song(ID) ON DELETE CASCADE,
-	FOREIGN KEY LabelID REFERENCES Label(ID) ON DELETE CASCADE
+	FOREIGN KEY (SongID) REFERENCES Song(ID) ON DELETE CASCADE,
+	FOREIGN KEY (LabelID) REFERENCES Label(ID) ON DELETE CASCADE
 	);
 	
 create table PublisherSong (
 	SongID integer,
 	PublisherID integer,
-	FOREIGN KEY SongID REFERENCES Song(ID) ON DELETE CASCADE,
-	FOREIGN KEY PublisherID REFERENCES Publisher(ID) ON DELETE CASCADE
+	FOREIGN KEY (SongID) REFERENCES Song(ID) ON DELETE CASCADE,
+	FOREIGN KEY (PublisherID) REFERENCES Publisher(ID) ON DELETE CASCADE
 	); 
 
 create table AlbumPerson (
 	AlbumID integer,
 	PersonID integer,
 	role varchar(10), --Mixer, Engineer, Producer, Performer, etc.
-	FOREIGN KEY AlbumID REFERENCES Album(ID) ON DELETE CASCADE,
-	FOREIGN KEY PersonID REFERENCES Person(ID) ON DELETE CASCADE
+	FOREIGN KEY (AlbumID) REFERENCES Album(ID) ON DELETE CASCADE,
+	FOREIGN KEY (PersonID) REFERENCES Person(ID) ON DELETE CASCADE
 	);
 	
 create table SongPerson (
 	SongID integer,
 	PersonID integer,
 	role varchar(10),
-	FOREIGN KEY SongID REFERENCES Song(ID) ON DELETE CASCADE,
-	FOREIGN KEY PersonID REFERENCES Person(ID) ON DELETE CASCADE
+	FOREIGN KEY (SongID) REFERENCES Song(ID) ON DELETE CASCADE,
+	FOREIGN KEY (PersonID) REFERENCES Person(ID) ON DELETE CASCADE
 	);
 	
 create table ArtistSong (
 	ArtistID integer,
 	SongID integer,
 	role varchar(20),
-	FOREIGN KEY ArtistID REFERENCES Artist(ID) ON DELETE CASCADE,
-	FOREIGN KEY SongID REFERENCES Song(ID) ON DELETE CASCADE
+	FOREIGN KEY (ArtistID) REFERENCES Artist(ID) ON DELETE CASCADE,
+	FOREIGN KEY (SongID) REFERENCES Song(ID) ON DELETE CASCADE
 	);
 	
 --Student table
